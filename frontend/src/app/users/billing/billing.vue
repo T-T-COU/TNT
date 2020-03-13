c0d314ef3864c229bf0850fb0802821e6e238bd4
morita657 <morita657@gmail.com>
2020-03-13 21:06:51 +0900

    decorate billing form

--- frontend/src/app/users/billing/billing.vue
+++ frontend/src/app/users/billing/billing.vue
@@ -1,19 +1,31 @@
 <template>
     <main role="main">
-    <section class="jumbotron text-center">
         <div class="container">
-        <h1>Input New Billing</h1>
-        <form action="" method="post">
-            <label for="amount">Amount</label>
-            <input class="amount" id="amount" type="text" placeholder="100円" />
-            <label for="name">Product Name</label>
-            <input class="name" id="name" type="text" placeholder="トイレットペーパー" />
-            <input type="file" name="resume" />
-            <input type="submit" value="Submit">
-        </form>
+            <h1>Input New Billing</h1>
+            <form action="/" method="post">
+                <div class="form-group">
+                    <label for="name">Product Name</label>
+                    <input class="form-control" id="name" type="text" placeholder="トイレットペーパー" />
+                </div>
+                <div class="form-group">
+                    <label for="amount">Amount</label>
+                    <input class="form-control" id="amount" type="text" placeholder="100円" />
+                </div>
+                <div class="form-group">
+                    <label for="house_name">House Name</label>
+                    <select class="form-control" name="" id="house_name">
+                        <option value="">Eifuku 1</option>
+                        <option value="">Eifuku 2</option>
+                        <option value="">Eifuku 3</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input type="file" class="form-control-file border" name="resume">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-    </section>
-    </main>
+    </main>    
 </template>
 
 <script>
